4bdba7ecfc43d26a730bc3740f79ca21ec555123
jlmaurer <maurer.jeremy@gmail.com>
2017-03-24 20:02:20 -0700

    Add make_dispG

--- setup_synthetic_problem.m
+++ setup_synthetic_problem.m
@@ -258,3 +258,25 @@ function [dd, GG, data, sig, SIG] = data_uncertainties(sd, G, dtru)
     data = dtru + sd*randn(size(dtru));
     dd = data./repmat(sig, 1, size(data,2)); 
 end
+
+function dispG=make_dispG(pm,xystats)
+
+xloc=[xystats';zeros(1,size(xystats',2))];
+
+npatches=size(pm,1);
+
+for k=1:npatches
+   m1=[pm(k,:) 1 0 0]';
+   m2=[pm(k,:) 0 1 0]';
+   
+   [U1,D,S]=disloc3d(m1,xloc,1,.25);
+   [U2,D,S]=disloc3d(m2,xloc,1,.25);
+   
+   G1(:,k)=U1(:);
+   G2(:,k)=U2(:);
+   
+ 
+end
+
+dispG=[G1 G2];
+end
